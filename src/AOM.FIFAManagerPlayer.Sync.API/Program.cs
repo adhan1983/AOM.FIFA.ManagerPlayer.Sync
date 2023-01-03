6afe4ff85ed3f76d4f27db6e43dc6692a06b96a7
Adhan Maldonado <adhan.maldonado@gmail.com>
2023-01-03 16:09:15 +0000

    azure pipeline

--- src/AOM.FIFAManagerPlayer.Sync.API/Program.cs
+++ src/AOM.FIFAManagerPlayer.Sync.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
 
 namespace AOM.FIFAManagerPlayer.Jobs.API
 {
@@ -15,6 +19,20 @@ namespace AOM.FIFAManagerPlayer.Jobs.API
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureAppConfiguration((context, config) => {
+
+                        var buildCofiguration = config.Build();
+                        string kvURL = buildCofiguration["FIFAMANAGERPLAYERSYNCKV:kvURL"];
+                        string tenantId = buildCofiguration["FIFAMANAGERPLAYERSYNCKV:TenantId"];
+                        string clientId = buildCofiguration["FIFAMANAGERPLAYERSYNCKV:ClientId"];
+                        string clientSecret = buildCofiguration["FIFAMANAGERPLAYERSYNCKV:ClientSecret"];
+
+                        var credentials = new ClientSecretCredential(tenantId, clientId, clientSecret);
+                        var client = new SecretClient(new System.Uri(kvURL), credentials);
+                        config.AddAzureKeyVault(client, new AzureKeyVaultConfigurationOptions());
+
+
+                    });
 
                 });
     }
